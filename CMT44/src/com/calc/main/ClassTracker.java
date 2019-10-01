75743c202d9012a7c3f69da81663918cd49818da
Anuradha Sampath Hewa Siribaddana <42120058+AnuradhaDev96@users.noreply.github.com>
2019-10-02 01:38:17 +0530

    Static values with class
    Main directory

---
+++ CMT44/src/com/calc/main/ClassTracker.java
@@ -0,0 +1,11 @@
+package com.calc.main;
+//Developer R.M.N.S.Rathnayke
+public class ClassTracker {
+	//cr store point
+		public static String classBlockStatus = "Closed";		
+		public static int classOpenBraCount = 0;
+		public static int classCloseBraCount = 0;
+		public static int detectedClassCount = 0;
+		
+		
+}
